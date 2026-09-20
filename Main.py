31616a9f570fa6bd726a62ca2bd8a129ae83dae3
rahim-compaore <kj9h4nmtcm@privaterelay.appleid.com>
2026-09-20 12:44:41 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,9 @@
-name = input("Wie heißt du? ")
-beruf = "Fachinformatiker"
-
-print(f"Hallo {name}!")
-print(f"Du wirst {beruf}.")
+jahre1 = 1100
+jahre2 = 1200
+jahre3 = 1300
+gesamt = (jahre1*12) + (jahre2*12) + (jahre3*12)
+print("dein gehalt in der ausbildung:")
+print(f"1. jahr: {jahre1} Euro * 12 = {jahre1*12} Euro")
+print(f"2. jahr: {jahre2} Euro * 12 = {jahre2*12} Euro")
+print(f"3. jahr: {jahre3} Euro * 12 = {jahre3*12} Euro")
+print(f"gesamt in 3 jahren: {gesamt} Euro")
